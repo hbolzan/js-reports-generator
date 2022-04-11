4c394a67ff94728d09593c5d54e68f7ae9bb338c
Henrique Bolzan <henrique@bolzan.com.br>
2022-04-10 22:02:23 -0300

    Make features switching work

--- src/components/feature/features-index.js
+++ src/components/feature/features-index.js
@@ -1,12 +1,22 @@
-async function itemClick(context, featureId) {
-    const feature = await context.features.get(featureId);
-    console.log(feature);
-}
-
 function FeaturesIndex(context) {
     const { config, api, views, httpClient, Dom } = context,
+          state = {},
           fetchOptions = { mode: "cors", errorMessage: "ATENÇÃO: O índice de funcionalidades não está disponível" },
           featuresUrl = config.apiUrl(api.features);
+
+    async function removeActive() {
+        if ( state.activeFeatureId ) {
+            const active = await context.features.get(state.activeFeatureId);
+            active.hide();
+        }
+    }
+
+    async function itemClick(context, featureId) {
+        const feature = await context.features.get(featureId);
+        await removeActive();
+        state.activeFeatureId = featureId;
+        feature.show();
+    }
 
     function index() {
         return httpClient.GET(featuresUrl, fetchOptions)
--- src/components/feature/features.js
+++ src/components/feature/features.js
@@ -1,23 +1,69 @@
-function Features(context) {
+const fetchOptions = {
+    mode: "cors",
+    errorMessage: "ATENÇÃO: A funcionalidade selecionada não está disponível"
+};
 
-    const cachedFeatures = {},
-          { httpClient, config, api } = context,
-          fetchOptions = {
-              mode: "cors",
-              errorMessage: "ATENÇÃO: A funcionalidade selecionada não está disponível"
+async function Feature(context, featureId) {
+    const { httpClient, config, api, Dom } = context,
+          node = context.document.getElementById(context.renderNodes.featuresBody),
+          state = {
+              definition: null,
+              doms: {},
+              active: null,
+              rendered: {},
           };
 
-    async function fetch(featureId) {
-        const featureUrl = config.apiUrl(api.feature(featureId));
-        return await httpClient.GET(featureUrl, fetchOptions);
+    function removeActive() {
+        if ( state.active ) {
+            node.removeChild(state.active);
+            state.active = null;
+            state.activeViewId = null;
+        }
     }
 
-    async function _get(featureId) {
-        if ( ! cachedFeatures[featureId] ) {
-            const response = await fetch(featureId);
-            cachedFeatures[featureId] = response.json();
+    async function fetch() {
+        const featureUrl = config.apiUrl(api.feature(featureId)),
+              feature = await httpClient.GET(featureUrl, fetchOptions);
+        return feature.json();
+    }
+
+    state.definition = await fetch();
+
+    function render(viewId) {
+        if ( ! state.doms[viewId] ) {
+            state.doms[viewId] = Dom(context, state.definition.views[viewId]);
         }
-        return cachedFeatures[featureId];
+    }
+
+    function show(requiredViewId) {
+        const viewId = requiredViewId || state.activeViewId || 0;
+        removeActive();
+        render(viewId);
+        if ( ! state.rendered[viewId] ) {
+            state.rendered[viewId] = state.doms[viewId].appendToDomNode(node);
+        } else {
+            node.appendChild(state.rendered[viewId]);
+        }
+        state.active = state.rendered[viewId];
+        state.activeViewId = viewId;
+    }
+
+    return {
+        state,
+        show,
+        hide: removeActive,
+    };
+}
+
+function Features(context) {
+
+    const cached = {};
+
+    async function _get(featureId) {
+        if ( ! cached[featureId] ) {
+            cached[featureId]= await Feature(context, featureId);
+        }
+        return cached[featureId];
     }
 
     return {
